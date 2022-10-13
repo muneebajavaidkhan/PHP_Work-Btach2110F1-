c226fed367b2baddb567cbd6d5fd012c04c024ed
Muneebakhan <muneebakhan430@gmail.com>
2022-10-13 18:14:03 +0500

    Updated Work 13-10-22

--- DbWork/Edit.php
+++ DbWork/Edit.php
@@ -7,20 +7,20 @@ $ID = $_GET['id'];
 $query = "select * from student where StdId = $ID";
 $getData = mysqli_query($con, $query);
 $res = mysqli_fetch_assoc($getData);
-
- $checkEdit = $res['Courses'];
-
- $checkBox = explode(',',$checkEdit );
-
- print_r($checkBox);
+$checkEdit = $res['Courses'];
+$checkBox = explode(',', $checkEdit);
+print_r($checkBox);
 ?>
 
 
 
 <div class="container"> <br>
-  <h3>Insert Data In DataBase</h3> <br>
+  <h3>Update Data In DataBase</h3> <br>
 
   <form action="insert.php" method = "post">
+
+
+    <input type="hidden"  name="StdID" value = <?= $res['StdId'] ?>  >
 
     <div class="form-group">
       <label for="name">StudentName:</label>
@@ -35,7 +35,9 @@ $res = mysqli_fetch_assoc($getData);
             <label class="form-check-label">
               Male: <input type="radio" class="form-check-input" name="gen" value = "Male"
               
-                <?php if($res['Gender'] == 'Male') {  echo 'checked'; }  ?>
+                <?php if ($res['Gender'] == 'Male') {
+                    echo 'checked';
+                } ?>
               >
             </label>
           </div>
@@ -43,7 +45,9 @@ $res = mysqli_fetch_assoc($getData);
             <label class="form-check-label">
               Female: <input type="radio" class="form-check-input" name="gen" value = "Female"
               
-              <?php if($res['Gender'] == 'Female') {  echo 'checked'; }  ?> 
+              <?php if ($res['Gender'] == 'Female') {
+                  echo 'checked';
+              } ?> 
               >
             </label>
           </div>
@@ -54,10 +58,18 @@ $res = mysqli_fetch_assoc($getData);
       <label for="city">Select City:</label>
       <select class="form-control" id="city" name = "city">
         <option value="">--Select--</option>
-        <option value = "Karachi"  <?php if($res['City'] == 'Karachi') {  echo 'selected'; }  ?>>Karachi</option>
-        <option value = "Lahore"  <?php if($res['City'] == 'Lahore') {  echo 'selected'; }  ?> >Lahore</option>
-        <option value = "Islamabad" <?php if($res['City'] == 'Islamabad') {  echo 'selected'; }  ?>>Islamabad</option>
-        <option value = "Peshawar" <?php if($res['City'] == 'Peshawar') {  echo 'selected'; }  ?>>Peshawar</option>
+        <option value = "Karachi"  <?php if ($res['City'] == 'Karachi') {
+            echo 'selected';
+        } ?>>Karachi</option>
+        <option value = "Lahore"  <?php if ($res['City'] == 'Lahore') {
+            echo 'selected';
+        } ?> >Lahore</option>
+        <option value = "Islamabad" <?php if ($res['City'] == 'Islamabad') {
+            echo 'selected';
+        } ?>>Islamabad</option>
+        <option value = "Peshawar" <?php if ($res['City'] == 'Peshawar') {
+            echo 'selected';
+        } ?>>Peshawar</option>
       </select>
     </div>
 
@@ -68,39 +80,49 @@ $res = mysqli_fetch_assoc($getData);
           <div class="form-check-inline ml-4">
             <label class="form-check-label">
               MVC <input type="checkbox" class="form-check-input" value="MVC" name = "Course[]"
-                <?php   if(in_array('MVC',$checkBox )){ echo 'checked'; }    ?>
+                <?php if (in_array('MVC', $checkBox)) {
+                    echo 'checked';
+                } ?>
               >
             </label>
           </div>
           <div class="form-check-inline">
             <label class="form-check-label">
               PHP <input type="checkbox" class="form-check-input" value="PHP" name = "Course[]"
-              <?php   if(in_array('PHP',$checkBox )){ echo 'checked'; }    ?>>
+              <?php if (in_array('PHP', $checkBox)) {
+                  echo 'checked';
+              } ?>>
             </label>
           </div>
           <div class="form-check-inline">
             <label class="form-check-label">
               MY SQL <input type="checkbox" class="form-check-input" value="MYSQL" name = "Course[]"
-              <?php   if(in_array('MYSQL',$checkBox )){ echo 'checked'; }    ?>>
+              <?php if (in_array('MYSQL', $checkBox)) {
+                  echo 'checked';
+              } ?>>
             </label>
           </div>
     </div>
 
     <div class="form-group">
       <label for="pwd">Education:</label>
-      <input type="text" class="form-control" id="edu" placeholder="Enter your Education" name="edu" value = <?= $res['Education']?>  >
+      <input type="text" class="form-control" id="edu" placeholder="Enter your Education" name="edu" value = <?= $res[
+          'Education'
+      ] ?>  >
     </div>
 
     <div class="form-group">
       <label for="fee">Fees:</label>
-      <input type="number" class="form-control" id="fee" placeholder="Enter your Fees" name="fee" value = <?= $res['Fees']?> >
+      <input type="number" class="form-control" id="fee" placeholder="Enter your Fees" name="fee" value = <?= $res[
+          'Fees'
+      ] ?> >
     </div>
 
 
 
     
 
-    <button type="submit" class="btn btn-primary" name = "ins">Submit</button>
+    <button type="submit" class="btn btn-primary" name = "Up">Update</button>
   </form>
 </div>
 
--- DbWork/insert.php
+++ DbWork/insert.php
@@ -3,6 +3,7 @@
 <?php include 'connection.php'; ?>
 
 
+<!-- insert Work -->
 
 <?php if (isset($_POST['ins'])) {
     $Name = $_POST['name'];
@@ -19,12 +20,34 @@
 
     if ($res) {
         echo "<script>alert('Data Inserted');window.location.href = 'ViewData.php'</script>";
-
-        
     } else {
         echo "<script>alert('Data Insertion Failed')</script>";
     }
 } ?>
 
+
+
+<!-- update Work -->
+<?php if (isset($_POST['Up'])) {
+    $stduentId = $_POST['StdID'];
+    $Name = $_POST['name'];
+    $Gender = $_POST['gen'];
+    $City = $_POST['city'];
+    $Education = $_POST['edu'];
+    $Fee = $_POST['fee'];
+    $Cor = $_POST['Course'];
+    $courses = implode(',', $Cor);
+
+    $query = "update student set StduentName = '$Name' ,Education = ' $Education ',Fees = '$Fee',
+    Gender = ' $Gender' ,City = '$City',Courses = '$courses' where StdId = '$stduentId'";
+
+    $res = mysqli_query($con, $query);
+
+    if ($res) {
+        echo "<script>alert('Data Updated');window.location.href = 'ViewData.php'</script>";
+    } else {
+        echo "<script>alert('Data Updation Failed')</script>";
+    }
+} ?>
 
 <?php include 'footer.php'; ?>
